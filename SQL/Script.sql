e7addaacdb8d46def391aa7a2a2db0fa5fc33ccb
matheosmt <simonet.matheo@gmail.com>
2025-12-10 16:12:10 +0100

    données dans le script SQL

--- SQL/Script.sql
+++ SQL/Script.sql
@@ -21,3 +21,22 @@ primary key (id_etudiant, nom_formation),
 foreign key (id_etudiant) references Etudiant(id_etudiant),
 foreign key (nom_formation) references Formation (nom_formation)
 );
+
+INSERT INTO Etudiant (id_etudiant, nom_etudiant, prenom_etudiant, mail_etudiant) values
+(1,"Tekin", "Melih", "tekinmelih@gmail.com"),
+(2, "Simonet", "Mathéo", "simonetmatheo@gmail.com"),
+(3, "Mohamed", "Fahmi", "mohamedfahmi@gmail.com"),
+(4, "Nguyen", "Donovan", "nguyendonovan@gmail.com");
+
+INSERT INTO Formation (nom_formation) values
+("Prepa ingenieur"),
+("Cycle ingenieur"),
+("Licence"),
+("BTS");
+
+INSERT INTO Inscription (id_etudiant, nom_formation) values
+(1, "BTS"),
+(2, "BTS"),
+(3, "Licence"),
+(4, "BTS");
+
